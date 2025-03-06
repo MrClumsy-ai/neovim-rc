e17bf43b5c2fbbd34216762325fe08e4a9b489d4
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-03-06 12:22:23 -0600

    modified:   after/plugin/treesitter.lua 	modified:   lua/eduardo/lazy.lua

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -4,6 +4,7 @@ config.setup({
 	ensure_installed = {"lua", "javascript", "c", "rust"},
 	sync_install = false,
 	highlight = {
+        enable = true,
 		additional_vim_regex_highlighting = false,
 	}
 })
--- lua/eduardo/lazy.lua
+++ lua/eduardo/lazy.lua
@@ -1,37 +1,37 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- PLUGINS
 local plugins = {
 
-	-- colorscheme
-	{ "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
+    -- colorscheme
+    { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
 
-	-- telescope
-	{
-		"nvim-telescope/telescope.nvim", tag = "0.1.8",
-		dependencies = { "nvim-lua/plenary.nvim" }
-	},
+    -- telescope
+    {
+        "nvim-telescope/telescope.nvim", tag = "0.1.8",
+        dependencies = { "nvim-lua/plenary.nvim" }
+    },
 
-	-- treesitter
-	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    -- treesitter
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
 
-	-- harpoon
-	{ "ThePrimeagen/harpoon" },
+    -- harpoon
+    { "ThePrimeagen/harpoon" },
 
     -- undotree
     { "mbbill/undotree" },
@@ -41,21 +41,21 @@ local plugins = {
 
     -- LSP
     { "VonHeikemen/lsp-zero.nvim" },
-	{ "neovim/nvim-lspconfig" },
-	{ "williamboman/mason.nvim" },
-	{ "williamboman/mason-lspconfig.nvim" },
+    { "neovim/nvim-lspconfig" },
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
 
-	-- completion
-	{ "hrsh7th/nvim-cmp" },
-	{ "hrsh7th/cmp-buffer" },
-	{ "hrsh7th/cmp-path" },
-	{ "saadparwaiz1/cmp_luasnip" },
-	{ "hrsh7th/cmp-nvim-lsp" },
-	{ "hrsh7th/cmp-nvim-lua" },
+    -- completion
+    { "hrsh7th/nvim-cmp" },
+    { "hrsh7th/cmp-buffer" },
+    { "hrsh7th/cmp-path" },
+    { "saadparwaiz1/cmp_luasnip" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/cmp-nvim-lua" },
 
     -- snippets
-	{ "L3MON4D3/LuaSnip" },
-	{ "rafamadriz/friendly-snippets" },
+    { "L3MON4D3/LuaSnip" },
+    { "rafamadriz/friendly-snippets" },
 
     -- rust
     {
