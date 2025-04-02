acab13c0f46241533950287289228789a5bbcc5a
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-04-02 13:52:21 -0600

    feat: timeout on yank 150

--- lua/eduardo/set.lua
+++ lua/eduardo/set.lua
@@ -30,6 +30,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking text",
 	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
 	callback = function()
-		vim.highlight.on_yank()
+		vim.highlight.on_yank({ timeout = 150 })
 	end,
 })
