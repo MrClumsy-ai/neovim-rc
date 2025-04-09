b18b0b3ecc170c0486be31aa05441199077af126
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-04-08 23:33:40 -0600

    feat: fugitive autopull every 5 mins

--- after/plugin/utilities/fugitive.lua
+++ after/plugin/utilities/fugitive.lua
@@ -1,2 +1,11 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>gp", ":Git push<CR>")
+
+local timer = vim.loop.new_timer()
+timer:start(
+	0,
+	300000,
+	vim.schedule_wrap(function()
+		vim.cmd("Git pull")
+	end)
+)
