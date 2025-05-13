472aaf63008fcf88ae70125d9d7d79decd135358
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-05-13 13:59:20 -0600

    deleted repeated git pulls

--- after/plugin/utilities/fugitive.lua
+++ after/plugin/utilities/fugitive.lua
@@ -1,13 +1,2 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>gp", ":Git push<CR>")
-
-local timer = vim.loop.new_timer()
-timer:start(
-	0,
-	300000,
-	vim.schedule_wrap(function()
-		if vim.fn.system("git diff --quiet HEAD") == 0 then
-			vim.cmd("Git pull")
-		end
-	end)
-)
